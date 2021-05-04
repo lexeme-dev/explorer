0b3ca4786661bf1b61ea31e94f5bb29a6faaccc2
Faiz Surani <faiz.surani@gmail.com>
2021-05-03 21:41:21 -0700

    Refactor App component and add onCaseSelected method

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import CaseSearch from "./CaseSearch";
+import Opinion from "../interfaces/Opinion";
 
-function App() {
-    return (
-        <div className="App">
-            <div className={"search-box"}>
-                <CaseSearch />
+export type OnCaseSelected = (opinion: Opinion) => void
+
+type AppState = {
+    selectedCases: Opinion[]
+}
+class App extends Component<{}, AppState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {selectedCases: []};
+    }
+
+
+    onCaseSelected: OnCaseSelected = (opinion) => {
+        this.setState(prevState => {
+            return {selectedCases: prevState.selectedCases.concat(opinion)}
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="search-box">
+                    <CaseSearch onCaseSelected={this.onCaseSelected}/>
+                </div>
+                <div className="selected-cases">
+                    <h2>Currently Selected Cases</h2>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default App;
