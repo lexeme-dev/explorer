f858ea755e7dcc5e4dd2f10dca50ff190a6c62a7
Faiz Surani <faiz.surani@gmail.com>
2021-05-04 11:32:10 -0700

    Refactor services into BaseService and non-static CaseService

---
+++ src/services/BaseService.ts
@@ -0,0 +1,16 @@
+import axios, {AxiosInstance} from "axios";
+import {API_URL} from "./ServiceConstants";
+import qs from "qs"
+
+class BaseService {
+    axios: AxiosInstance
+
+    constructor() {
+        this.axios = axios.create({
+            baseURL: API_URL,
+            paramsSerializer: params => qs.stringify(params, {arrayFormat: "repeat"})
+        })
+    }
+}
+
+export default BaseService;
