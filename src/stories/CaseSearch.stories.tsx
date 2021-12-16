e3874648c11947937b5ac54f27ee98558031a18d
Varun Iyer <varun_iyer@posteo.net>
2021-12-16 12:42:28 -0800

    Added CaseSearch story with actions

---
+++ src/stories/CaseSearch.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import CaseSearch from '../components/CaseSearch';
+
+import Opinion from '../interfaces/Opinion';
+
+export default {
+  title: 'Search/CaseSearch',
+  component: CaseSearch,
+} as ComponentMeta<typeof CaseSearch>;
+
+const Template: ComponentStory<typeof CaseSearch> = (args) => <CaseSearch {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  selectedCases: [],
+  onCaseSelected: action('clicked'),
+};
