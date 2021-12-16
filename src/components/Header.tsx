39102f4b8c3bb7be2f2ffad1da5f787bc2794477
Varun Iyer <varun_iyer@posteo.net>
2021-12-16 01:16:02 -0800

    Added Storybook defaults

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.scss';
+import './Header.scss';
+import './main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export type HeaderProps = {
+};
+export type HeaderState = {
+};
+
+class Header extends Component<HeaderProps, HeaderState> {
+    /* I'm leaving this as a Class because we will need state information here
+     * soon.
+     */
+    render() {
+        return (
+			<nav id="explorerNav" className="navbar navbar-inverse py-0 bg-primary">
+			  <a className="h1 text-light font-weight-bolder" href="#">explorer</a>
+			</nav>
+        );
+    }
+}
+
+export default Header;
