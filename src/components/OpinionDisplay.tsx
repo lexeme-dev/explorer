755dbd3067c73ec041babde156541ac5447d9c60
Varun Iyer <varun_iyer@posteo.net>
2021-12-18 01:02:37 -0800

    Added simple OpinionDisplay (to be complicated later)

---
+++ src/components/OpinionDisplay.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Opinion, { fullCaseName } from '../interfaces/Opinion'
+import './OpinionDisplay.scss';
+import './main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+type OpinionDisplayProps = {
+    opinion: Opinion;
+}
+
+function OpinionDisplay(props: OpinionDisplayProps) {
+    return <div> { fullCaseName(props.opinion) } </div>;
+}
+
+export default OpinionDisplay;
