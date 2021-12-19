3b190a8d65059e2d76beff15b0286c4bf4efc5c7
Varun Iyer <varun_iyer@posteo.net>
2021-12-18 20:20:20 -0800

    Added trash buttons after much difficulty

--- src/components/SelectedCaseList.tsx
+++ src/components/SelectedCaseList.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './SelectedCaseList.scss';
+import { Trash, XSquare } from 'react-bootstrap-icons';
 import './main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Opinion, { fullCaseName } from '../interfaces/Opinion';
@@ -27,9 +28,20 @@ class SelectedCaseList extends Component<SelectedCaseListProps, SelectedCaseList
         return (
 			<div id="selected-case-list">
 				<h5 id="selected-case-list-header"> Bookmarked Cases </h5>
-				{ this.state.selectedCases.map((opinion) => (
-					<OpinionDisplay opinion={ opinion } />
-				)) }
+				<ul className="list-group justify-content-center">
+					{ this.state.selectedCases.map((opinion) => (
+						<li className="list-group-item selected-case-li">
+							<div className="flex-selected-case-list-item">
+								<div className="selected-case-list-item-name">
+									<OpinionDisplay opinion={ opinion } />
+								</div>
+								<div className="selected-case-list-item-actions">
+									<button type="button" className="btn btn-danger btn-sm selected-case-list-trash-btn"> <Trash /> </button>
+								</div> 
+							</div>
+						</li>
+					)) }
+				</ul>
 			</div>
         );
     }
